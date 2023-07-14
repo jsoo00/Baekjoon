106255578854cde098e83cfea04a32231f049902
JSO0o <111573326+JSO0o@users.noreply.github.com>
2023-07-14 15:49:11 +0900

    [Silver IV] Title: 수 찾기, Time: 72 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -5,42 +5,22 @@ using namespace std;
 
 int main(void) {
 	int N, M;
-
-	//N입력
-	scanf("%d", &N);
-
-	//A입력
-	vector<int> A(N);
+	scanf("%d", &N); //N입력
+	vector<int> A(N);//A입력
 	for (int i = 0; i < N; i++) {
 		scanf("%d", &A[i]);
 	}
-
-	//A 오름차순 정렬
-	sort(A.begin(), A.end());
-
-
-	//M입력
-	scanf("%d", &M);
-
-	//B입력
-	vector<int> B(M);
+	sort(A.begin(), A.end());//A 오름차순 정렬
+	scanf("%d", &M);//M입력
+	vector<int> B(M);//B입력
 	for (int i = 0; i < M; i++) {
 		scanf("%d", &B[i]);
 	}
-
-	//B 오름차순 정렬
-	//sort(B.begin(), B.end());
-
 	vector<int> result; //출력 값
-
-
-
-	//for문을 통해 숫자가 존재하는지 검사한다.
-	for (int i = 0; i < M; i++) {
+	
+	for (int i = 0; i < M; i++) {//for문을 통해 숫자가 존재하는지 검사한다.
 		int target = B[i];
-
-	// binary_search(반복자.시작점, 반복자.끝점, 찾고자 하는 값);
-	//찾고자 하는 값을 찾으면 true를, 찾지 못하면 false를 반환한다.
+	// binary_search(반복자.시작점, 반복자.끝점, 찾고자 하는 값); 찾으면 true를,  못 찾으면false.
 		bool isFound = binary_search(A.begin(), A.end(), target);
 			if (isFound) {
 				result.push_back(1);
