c672c6e910411b5e85dce0156b2af9b3c57beac8
JSO0o <111573326+JSO0o@users.noreply.github.com>
2023-07-30 23:33:30 +0900

    [Silver IV] Title: 점화식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13699. 점화식/점화식.cc
+++ 백준/Silver/13699. 점화식/점화식.cc
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+int main(void) {
 	int n;
-	long long t[36] = { 0, }; //0으로 초기화
-	//N의 범위는 0부터 35까지이므로 16번째 줄에서 t[35]를 접근하기 위해서는 t의 크기가 최소 36 이상이어야 한다.
 	cin >> n;
+
+	vector<long long>t(n+1);
 
 	t[0] = 1;
 
@@ -14,7 +15,6 @@ int main() {
 			t[i] += t[j] * t[i - j - 1];
 		}
 	}
-
 	cout << t[n];
 
 	return 0;
